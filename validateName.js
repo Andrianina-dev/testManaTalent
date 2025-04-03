cc6bc63eea6e26cefdad347e0c73fbd1c3a4852b
andrianinaDev <andrianinarandria001@hotmail.com>
2025-04-03 14:36:47 +0300

    effacez

--- validateName.js
+++
@@ -1,7 +0,0 @@
-module.exports = function validateName(req, res, next) {
-    if (!req.body.name) {
-      return res.status(400).json({ error: 'Le champ name est obligatoire' });
-    }
-    next();
-  };
-  
